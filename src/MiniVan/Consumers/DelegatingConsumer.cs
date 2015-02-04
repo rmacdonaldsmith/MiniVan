7854302b5f0171b934e9dd95311ef0af21d72ecf
Robert Macdonald Smith <rmacdonaldsmith@yahoo.co.uk>
2015-02-03 21:37:38 -0800

    moved delegating consumer to its own class file

---
+++ src/MiniVan/Consumers/DelegatingConsumer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MiniVan.Consumers
+{
+	public class DelegatingConsumer<T> : IConsume<T> where T : IMessage
+	{
+		private readonly Action<T> _delegate;
+
+		public DelegatingConsumer (Action<T> delegatingAction)
+		{
+			Ensure.NotNull (delegatingAction, "delegatingAction");
+			_delegate = delegatingAction;
+		}
+
+		public void Handle (T msg)
+		{
+			_delegate (msg);
+		}
+	}
+}
+
--- src/MiniVan/Consumers/ObservableConsumer.cs
+++ src/MiniVan/Consumers/ObservableConsumer.cs
@@ -7,22 +7,6 @@ using System.Reactive.Subjects;
 
 namespace MiniVan.Consumers
 {
-	public class DelegatingConsumer<T> : IConsume<T> where T : IMessage
-	{
-		private readonly Action<T> _delegate;
-
-		public DelegatingConsumer (Action<T> delegatingAction)
-		{
-			Ensure.NotNull (delegatingAction, "delegatingAction");
-			_delegate = delegatingAction;
-		}
-
-		public void Handle (T msg)
-		{
-			_delegate (msg);
-		}
-	}
-		
 	public class ObservableConsumerWrapper<T> : IObservable<T> where T : IMessage
 	{
 		private readonly IConsume<T> _innerConsumer;
